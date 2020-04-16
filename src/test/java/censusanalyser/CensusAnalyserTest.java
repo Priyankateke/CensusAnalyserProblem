e8f0aec4433cab3349a6838bd3c6a8ed90bcc432
Priyankateke <priiyankateke1705@gmail.com>
2020-04-16 06:05:36 -0700

    ADDED: Given Census CSV File Type Is Incorrect Return Exception

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -8,6 +8,7 @@ public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH="./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_FILE_TYPE="./src/test/resources/IndiaStateCensusData.json";
 
     /* TC 1.1 : Given the States Census CSV file, Check to ensure the Number of Record matches */
     @Test
@@ -16,7 +17,9 @@ public class CensusAnalyserTest {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29,numOfRecords);
-        } catch (CensusAnalyserException e) { }
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
     }
 
     /* TC 1.2 : Given the State Census CSV File if incorrect Returns a custom Exception */
@@ -27,6 +30,20 @@ public class CensusAnalyserTest {
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CensusAnalyserException.class);
             censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    /* TC 1.3 : Given the State Census CSV File when correct
+    but type incorrect Returns a custom Exception */
+    @Test
+    public void givenStateCensusCsvFile_WhenTypeIncorrect_shouldReturnException() {
+        try {
+            CensusAnalyser censusAnalyzer = new CensusAnalyser();
+            ExpectedException expectedException = ExpectedException.none();
+            expectedException.expect(CensusAnalyserException.class);
+            censusAnalyzer.loadIndiaCensusData(WRONG_FILE_TYPE);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
